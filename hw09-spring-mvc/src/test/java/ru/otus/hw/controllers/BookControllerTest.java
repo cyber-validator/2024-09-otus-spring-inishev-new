3a203aa656f1f6b63c5731ecbb5894468a184d5e
illusion <the.validator@yandex.ru>
2025-03-20 20:48:44 +0300

    extract creating authors and genres to methods

--- hw09-spring-mvc/src/test/java/ru/otus/hw/controllers/BookControllerTest.java
+++ hw09-spring-mvc/src/test/java/ru/otus/hw/controllers/BookControllerTest.java
@@ -20,6 +20,7 @@ import ru.otus.hw.services.GenreService;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -43,8 +44,14 @@ class BookControllerTest {
     @MockBean
     private GenreService genreService;
 
+    private List<Author> authors;
+
+    private List<Genre> genres;
+
     @BeforeEach
     void setUp() {
+        authors = getAuthors();
+        genres = getGenres();
     }
 
     @AfterEach
@@ -53,23 +60,8 @@ class BookControllerTest {
 
     @Test
     void shouldListAllBooks() throws Exception {
-        Author author = new Author();
-        author.setId(1);
-        author.setFullName("Author 1");
-
-        Genre genre = new Genre();
-        genre.setId(1);
-        genre.setName("Genre 1");
-
-        Book book = new Book();
-        book.setTitle("Book Title");
-        book.setAuthor(author);
-        book.setGenres(List.of(genre));
-
-        Book book2 = new Book();
-        book2.setTitle("Book 2 Title");
-        book2.setAuthor(author);
-        book2.setGenres(List.of(genre));
+        Book book = createBook("Book Title 1", authors.get(0), genres);
+        Book book2 = createBook("Book Title 2", authors.get(0), List.of(genres.get(0)));
 
         when(bookService.findAll()).thenReturn(List.of(book, book2));
         mvc.perform(get("/"))
@@ -81,44 +73,21 @@ class BookControllerTest {
 
     @Test
     void shouldOpenBookCreatingPage() throws Exception {
-        Author author = new Author();
-        author.setId(1);
-        author.setFullName("Author 1");
-
-        Genre genre = new Genre();
-        genre.setId(1);
-        genre.setName("Genre 1");
-
-        when(authorService.findAll()).thenReturn(List.of(author));
-        when(genreService.findAll()).thenReturn(List.of(genre));
+        when(authorService.findAll()).thenReturn(authors);
+        when(genreService.findAll()).thenReturn(genres);
 
         mvc.perform(get("/create-new-book"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("save"))
                 .andExpect(model().attributeExists("book"))
-                .andExpect(model().attribute("authors", List.of(author)))
-                .andExpect(model().attribute("genres", List.of(genre)));
+                .andExpect(model().attribute("authors", authors))
+                .andExpect(model().attribute("genres", genres));
     }
 
     @Test
     void shouldSaveEditedBook() throws Exception {
-        Author author = new Author();
-        author.setId(1);
-        author.setFullName("Author 1");
-
-        Genre genre = new Genre();
-        genre.setId(1);
-        genre.setName("Genre 1");
-
-        Book original = new Book();
-        original.setTitle("Book Title");
-        original.setAuthor(author);
-        original.setGenres(List.of(genre));
-
-        Book modified = new Book();
-        modified.setTitle("Edited Book Title");
-        modified.setAuthor(author);
-        modified.setGenres(List.of(genre));
+        Book original = createBook("Book Title Original", authors.get(0), List.of(genres.get(0)));
+        Book modified = createBook("Book Title Edited", authors.get(1), List.of(genres.get(0)));
 
         when(bookService.findById(1L)).thenReturn(Optional.of(original));
         when(bookService.findAll()).thenReturn(List.of(modified));
@@ -135,23 +104,15 @@ class BookControllerTest {
                 .andExpect(view().name("redirect:/"));
 
         verify(bookService, times(1))
-                .update(modified.getId(), modified.getTitle(), modified.getAuthor().getId(), Set.of(genre.getId()));
+                .update(modified.getId(),
+                        modified.getTitle(),
+                        modified.getAuthor().getId(),
+                        Set.of(modified.getGenres().get(0).getId()));
     }
 
     @Test
     void shouldCreateNewBook() throws Exception {
-        Author author = new Author();
-        author.setId(1);
-        author.setFullName("Author 1");
-
-        Genre genre = new Genre();
-        genre.setId(1);
-        genre.setName("Genre 1");
-
-        Book book = new Book();
-        book.setTitle("Created Book Title");
-        book.setAuthor(author);
-        book.setGenres(List.of(genre));
+        Book book = createBook("Created Book Title", authors.get(2), List.of(genres.get(0)));
 
         mvc.perform(post("/save")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
@@ -165,7 +126,10 @@ class BookControllerTest {
                 .andExpect(view().name("redirect:/"));
 
         verify(bookService, times(1))
-                .update(book.getId(), book.getTitle(), book.getAuthor().getId(), Set.of(genre.getId()));
+                .update(book.getId(),
+                        book.getTitle(),
+                        book.getAuthor().getId(),
+                        Set.of(book.getGenres().get(0).getId()));
     }
 
     @Test
@@ -180,26 +144,15 @@ class BookControllerTest {
 
     @Test
     void shouldReturnValidationErrorsOnCreateNewBookWithEmptyTitle() throws Exception {
-        Author author = new Author();
-        author.setId(1);
-        author.setFullName("Author 1");
+        Book book = createBook("Created Book Title", authors.get(0), List.of(genres.get(0)));
 
-        Genre genre = new Genre();
-        genre.setId(1);
-        genre.setName("Genre 1");
-
-        Book book = new Book();
-        book.setTitle("Created Book Title");
-        book.setAuthor(author);
-        book.setGenres(List.of(genre));
-
-        when(authorService.findAll()).thenReturn(List.of(author));
-        when(genreService.findAll()).thenReturn(List.of(genre));
+        when(authorService.findAll()).thenReturn(authors);
+        when(genreService.findAll()).thenReturn(genres);
 
         mvc.perform(post("/save")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                         .content("id=" + book.getId() +
-                                "&title="  +
+                                "&title=" +
                                 "&authorId=" + book.getAuthor().getId() +
                                 "&genreIds=" + book.getGenres().get(0).getId() +
                                 "&_genreIds=on&_genreIds=on&_genreIds=on" +
@@ -208,11 +161,34 @@ class BookControllerTest {
                 .andExpect(model().hasErrors())
                 .andExpect(view().name("save"))
                 .andExpect(model().attributeExists("book"))
-                .andExpect(model().attribute("authors", List.of(author)))
-                .andExpect(model().attribute("genres", List.of(genre)));
+                .andExpect(model().attribute("authors", authors))
+                .andExpect(model().attribute("genres", genres));
 
         verify(bookService, times(0))
-                .update(book.getId(), book.getTitle(), book.getAuthor().getId(), Set.of(genre.getId()));
+                .update(book.getId(),
+                        book.getTitle(),
+                        book.getAuthor().getId(),
+                        Set.of(book.getGenres().get(0).getId()));
+    }
+
+    private static List<Author> getAuthors() {
+        return IntStream.range(1, 4).boxed()
+                .map(id -> new Author(id, "Author_" + id))
+                .toList();
+    }
+
+    private static List<Genre> getGenres() {
+        return IntStream.range(1, 4).boxed()
+                .map(id -> new Genre(id, "Genre_" + id))
+                .toList();
+    }
+
+    private Book createBook(String title, Author author, List<Genre> genres) {
+        Book book = new Book();
+        book.setTitle(title);
+        book.setAuthor(author);
+        book.setGenres(genres);
+        return book;
     }
 
 }
