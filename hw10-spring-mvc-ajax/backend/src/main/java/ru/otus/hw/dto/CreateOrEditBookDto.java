445483e92fb929323436dd0aa388ce897b9a0d12
illusion <the.validator@yandex.ru>
2025-06-08 22:44:23 +0300

    add lombok annotations

--- hw10-spring-mvc-ajax/backend/src/main/java/ru/otus/hw/dto/CreateOrEditBookDto.java
+++ hw10-spring-mvc-ajax/backend/src/main/java/ru/otus/hw/dto/CreateOrEditBookDto.java
@@ -2,12 +2,15 @@ package ru.otus.hw.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import ru.otus.hw.models.Book;
-import ru.otus.hw.models.Genre;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@Getter
+@Setter
 public class CreateOrEditBookDto {
 
     private long id;
@@ -19,50 +22,5 @@ public class CreateOrEditBookDto {
 
     @NotEmpty(message = "Need to select at least one genre")
     private List<Long> genreIds;
-
-    public CreateOrEditBookDto() {
-    }
-
-    public CreateOrEditBookDto(Book b) {
-        this.id = b.getId();
-        this.title = b.getTitle();
-        this.authorId = b.getAuthor().getId();
-        this.genreIds = new ArrayList<>();
-        for (Genre g: b.getGenres()) {
-            this.genreIds.add(g.getId());
-        }
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public long getAuthorId() {
-        return authorId;
-    }
-
-    public void setAuthorId(long authorId) {
-        this.authorId = authorId;
-    }
-
-    public List<Long> getGenreIds() {
-        return genreIds;
-    }
-
-    public void setGenreIds(List<Long> genreIds) {
-        this.genreIds = genreIds;
-    }
 
 }
