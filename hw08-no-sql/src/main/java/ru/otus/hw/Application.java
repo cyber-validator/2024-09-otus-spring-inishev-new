d8b39047777a3467ac5d1e7252a79f3d72672c1f
illusion <the.validator@yandex.ru>
2025-06-24 19:41:32 +0300

    remove sql exception

--- hw08-no-sql/src/main/java/ru/otus/hw/Application.java
+++ hw08-no-sql/src/main/java/ru/otus/hw/Application.java
@@ -5,14 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import java.sql.SQLException;
-
 @EnableMongock
 @EnableMongoRepositories
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
