8539b0a192b9d0f898d22261ac5003568fe76b57
illusion <the.validator@yandex.ru>
2025-06-24 15:54:25 +0300

    rename service

--- hw18-resilence/client-service/src/main/java/ru/otus/hw/controller/BookController.java
+++ hw18-resilence/client-service/src/main/java/ru/otus/hw/controller/BookController.java
@@ -3,13 +3,13 @@ package ru.otus.hw.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.otus.hw.services.ExtRestService;
+import ru.otus.hw.services.ExtLibraryService;
 
 @RestController
 @AllArgsConstructor
 public class BookController {
 
-    private final ExtRestService service;
+    private final ExtLibraryService service;
 
     @GetMapping("/")
     public String getAllBooks() {
--- hw18-resilence/client-service/src/main/java/ru/otus/hw/services/ExtLibraryService.java
+++ hw18-resilence/client-service/src/main/java/ru/otus/hw/services/ExtLibraryService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 @Service
-public class ExtRestService {
+public class ExtLibraryService {
 
     private static final String SERVICE_NAME = "libService";
 
