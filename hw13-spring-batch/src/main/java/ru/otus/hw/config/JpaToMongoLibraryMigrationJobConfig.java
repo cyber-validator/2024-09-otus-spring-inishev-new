71d058ba079eb61207a73fd60d02755aa573b9d6
illusion <the.validator@yandex.ru>
2025-06-29 00:28:49 +0300

    add job

---
+++ hw13-spring-batch/src/main/java/ru/otus/hw/config/JpaToMongoLibraryMigrationJobConfig.java
@@ -0,0 +1,30 @@
+package ru.otus.hw.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.otus.hw.JobCompletionNotificationListener;
+
+@Configuration
+@RequiredArgsConstructor
+public class JpaToMongoLibraryMigrationJobConfig {
+
+    private static final String JOB_NAME = "postgresToMongoLibraryMigrationJob";
+
+    @Bean
+    public Job sampleJob(JobRepository jobRepository,
+                         Step bookStep,
+                         Step commentStep,
+                         JobCompletionNotificationListener listener) {
+        return new JobBuilder(JOB_NAME, jobRepository)
+                .listener(listener)
+                .start(bookStep)
+                .next(commentStep)
+                .build();
+    }
+
+}
