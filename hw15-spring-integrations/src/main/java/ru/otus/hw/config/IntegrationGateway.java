cfd899b682cb04616027ec52e91f91c29cf06896
illusion <the.validator@yandex.ru>
2025-06-29 22:12:13 +0300

    update IntegrationGateway

--- hw15-spring-integrations/src/main/java/ru/otus/hw/config/IntegrationGateway.java
+++ hw15-spring-integrations/src/main/java/ru/otus/hw/config/IntegrationGateway.java
@@ -2,13 +2,23 @@ package ru.otus.hw.config;
 
 import org.springframework.integration.annotation.Gateway;
 import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.messaging.handler.annotation.Payload;
 import ru.otus.hw.model.Body;
 import ru.otus.hw.model.Vehicle;
 
 @MessagingGateway
 public interface IntegrationGateway {
 
-    @Gateway(requestChannel = "inputChannel", replyChannel = "outputChannel")
-    Vehicle process(Body template);
+    @Gateway(requestChannel = "assembleVehicleInputChannel", replyChannel = "assembleVehicleOutputChannel")
+    Vehicle produceVehicle(@Payload String vehicleIdentificationNumber);
+
+    /*@Gateway(requestChannel = "bodyChannel")
+    Vehicle processBody(@Payload Body body);
+
+    @Gateway(requestChannel = "engineChannel")
+    Vehicle processEngine(@Payload Vehicle vehicle);
+
+    @Gateway(requestChannel = "equipmentChannel")
+    Vehicle processEquipment(@Payload Vehicle vehicle);*/
 
 }
