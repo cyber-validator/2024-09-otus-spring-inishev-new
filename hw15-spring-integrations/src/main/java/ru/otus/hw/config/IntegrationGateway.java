0070b11ccfab60247b46428d6b8917f3faca2006
illusion <the.validator@yandex.ru>
2025-06-29 22:52:22 +0300

    remove unnecessary

--- hw15-spring-integrations/src/main/java/ru/otus/hw/config/IntegrationGateway.java
+++ hw15-spring-integrations/src/main/java/ru/otus/hw/config/IntegrationGateway.java
@@ -3,7 +3,6 @@ package ru.otus.hw.config;
 import org.springframework.integration.annotation.Gateway;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.messaging.handler.annotation.Payload;
-import ru.otus.hw.model.Body;
 import ru.otus.hw.model.Vehicle;
 
 @MessagingGateway
@@ -11,14 +10,5 @@ public interface IntegrationGateway {
 
     @Gateway(requestChannel = "assembleVehicleInputChannel", replyChannel = "assembleVehicleOutputChannel")
     Vehicle produceVehicle(@Payload String vehicleIdentificationNumber);
-
-    /*@Gateway(requestChannel = "bodyChannel")
-    Vehicle processBody(@Payload Body body);
-
-    @Gateway(requestChannel = "engineChannel")
-    Vehicle processEngine(@Payload Vehicle vehicle);
-
-    @Gateway(requestChannel = "equipmentChannel")
-    Vehicle processEquipment(@Payload Vehicle vehicle);*/
 
 }
--- hw15-spring-integrations/src/main/java/ru/otus/hw/config/VehicleIntegrationConfig.java
+++ hw15-spring-integrations/src/main/java/ru/otus/hw/config/VehicleIntegrationConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.MessageChannelSpec;
 import org.springframework.integration.dsl.MessageChannels;
-import org.springframework.messaging.MessageChannel;
 import ru.otus.hw.service.VehicleAssembleService;
 
 @Configuration
@@ -18,21 +17,6 @@ public class VehicleIntegrationConfig {
         return MessageChannels.queue(QUEUE_CAPACITY);
     }
 
-//    @Bean
-//    public MessageChannel bodyChannel() {
-//        return MessageChannels.direct().getObject();
-//    }
-//
-//    @Bean
-//    public MessageChannel engineChannel() {
-//        return MessageChannels.direct().getObject();
-//    }
-//
-//    @Bean
-//    public MessageChannel equipmentChannel() {
-//        return MessageChannels.direct().getObject();
-//    }
-
     @Bean
     public MessageChannelSpec<?, ?> assembleVehicleOutputChannel() {
         return MessageChannels.publishSubscribe();
@@ -42,11 +26,8 @@ public class VehicleIntegrationConfig {
     public IntegrationFlow vehicleAssemblyFlow(VehicleAssembleService vehicleAssembleService) {
         return IntegrationFlow
                 .from(assembleVehicleInputChannel())
-//                .channel(bodyChannel())
                 .transform(vehicleAssembleService::addBody)
-//                .channel(engineChannel())
                 .transform(vehicleAssembleService::addEngine)
-//                .channel(equipmentChannel())
                 .transform(vehicleAssembleService::addEquipment)
                 .channel(assembleVehicleOutputChannel())
                 .get();
