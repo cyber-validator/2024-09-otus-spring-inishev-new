adb432b7c8e6244e25139c122e4549a1e7f951fe
illusion <the.validator@yandex.ru>
2025-06-29 22:11:14 +0300

    move Runner to another package

--- hw15-spring-integrations/src/main/java/ru/otus/hw/Runner.java
+++ hw15-spring-integrations/src/main/java/ru/otus/hw/Runner.java
@@ -1,10 +1,11 @@
-package ru.otus.hw.config;
+package ru.otus.hw;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import ru.otus.hw.model.Body;
+import ru.otus.hw.config.IntegrationGateway;
+import ru.otus.hw.service.VehicleIdentificationNumberGenerator;
 
 @Slf4j
 @Component
@@ -13,20 +14,21 @@ public class Runner implements CommandLineRunner {
 
     private final IntegrationGateway gateway;
 
+    private final VehicleIdentificationNumberGenerator vinGenerator;
+
     @Override
     public void run(String... args) {
-        for (byte i = 0; ; i++) {
+        for (long i = 0; ; i++) {
             sleep(2_000);
-            var res = gateway.process(new Body());
+            var res = gateway.produceVehicle(vinGenerator.getVin());
             log.info("Received data #{}: {}", i, res);
         }
     }
 
     private void sleep(int delay) {
         try {
-            log.info("=> SLEEP");
+            log.info("SLEEPING {} ms", delay);
             Thread.sleep(delay);
-            log.info("=> WAKE UP");
         } catch (InterruptedException ignored) {
         }
     }
